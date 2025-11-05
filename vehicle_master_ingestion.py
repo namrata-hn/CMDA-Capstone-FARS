5337349ba8ecef78340fa1c03bcb532b6b746b98
rafvray <124562520+rafvray@users.noreply.github.com>
2025-11-05 11:16:26 -0500

    Master.csv and ingestion scripts
    For FARS database in SQL Workbench, saved locally on computer

---
+++ vehicle_master_ingestion.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine, text, inspect # type: ignore
+import pandas as pd # type: ignore
+
+host = "localhost"
+port = 3306
+database = "fars"
+user = "root" # replace with your username
+password = "NewStrongPass!123"
+
+# Use this line if your MySQL user has a password
+engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}")
+# engine = create_engine(f"mysql+pymysql://{user}@{host}:{port}/{database}")
+
+try:
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT VERSION();"))
+        version = result.scalar()
+        print(f"MySQL version: {version}")
+except Exception as e:
+    print("Connection failed:", e)
+
+
+inspector = inspect(engine)
+tables = inspector.get_table_names()
+print("Tables in database:", tables)
+
+vehicle_master = pd.read_csv("/Users/rafaelviray/Downloads/master_vehicle.csv") # change to your path
+
+vehicle_master.head(0).to_sql(
+    name="vehicle_master",
+    con=engine,
+    if_exists="replace",  
+    index=False
+)
+
+vehicle_master.to_sql(
+    name="vehicle_master",
+    con=engine,
+    if_exists="append",  # now insert data
+    index=False,
+    chunksize=1000      
+)
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT COUNT(*) FROM vehicle_master;"))
+    count = result.scalar() 
+    print(f"Rows now in table: {count}")
